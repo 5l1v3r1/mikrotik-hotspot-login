96bb7124cbc08317395111732907e7028244baae
Om3rCitak <mail@omercitak.com>
2016-11-28 18:04:42 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# mikrotik-hotspot-login
+# Mikrotik Hotspot Login
 This is hotspot login template for Mikrotik systems. 
+
+- Coding for [FlyFiber](http://www.flyfiber.com.tr/)
+- Miktotik:  <http://www.mikrotik.com/>
---
+++ js/om3r.js
@@ -0,0 +1,12 @@
+/*
+	Document	: om3r.js - Fly Fiber Hotspot
+	Author		: Ömer Çıtak - www.omercitak.net
+	Create		: 17.04.2014
+*/
+
+$(document).ready(function(){
+	$(".paket1, .paket2").hide();
+	$(".paket1, .paket2").fadeIn(2000).show();
+	Cufon.replace("header .ustyazi span");
+	Cufon.replace("#slider .login .satir1");
+});
